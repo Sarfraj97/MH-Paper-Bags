3e3354a4839c9fffbac08243751d4bfaef8bd2a5
Sarfraj97 <szkkhan2@gmail.com>
2023-12-07 20:50:06 +0530

    updated proc\file

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module MHPaperBags
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "mh-paper-bags-production.up.railway.app"
   end
 end
